8b61ee57c4012a064126d85f4ede95df1686d793
Senjam RatnaKumar <sratnakumar99@gmail.com>
2022-05-29 22:04:33 +0530

    adding state to cartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,17 +1,48 @@
 import React from 'react';
-
 class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: 'Phone',
+      price: 9999,
+      qty: 20,
+      img: '',
+    };
+  }
+
   render() {
+    const { title, price, qty } = this.state;
     return (
       <div className='cart-item'>
         <div className='left-block'>
-          <img style={styles.image} />
+          <img
+            src='https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-x-new-1.jpg'
+            style={styles.image}
+            alt='img'
+          />
         </div>
         <div className='right-block'>
-          <div style={styles.font}>Phone</div>
-          <div style={styles.color}>Rs 9999</div>
-          <div style={styles.color}>Qty: 1</div>
-          <div className='cart-item-actions'>{/* buttons */}</div>
+          <div style={styles.font}>{title}</div>
+          <div style={styles.color}>Rs {price}</div>
+          <div style={styles.color}>Qty: {qty}</div>
+          <div className='cart-item-actions'>
+            {/* buttons */}
+            <img
+              src='https://cdn-icons-png.flaticon.com/512/992/992651.png'
+              alt='increase'
+              className='action-icons'
+            />
+            <img
+              src='https://cdn-icons-png.flaticon.com/512/992/992683.png'
+              alt='decrease'
+              className='action-icons'
+            />
+            <img
+              src='https://cdn-icons.flaticon.com/png/512/2907/premium/2907762.png?token=exp=1653842129~hmac=78d1c4e4f266acfa1a5ecea1e94f3c99'
+              alt='delete'
+              className='action-icons'
+            />
+          </div>
         </div>
       </div>
     );
@@ -20,8 +51,8 @@ class CartItem extends React.Component {
 
 var styles = {
   image: {
-    width: 110,
-    height: 110,
+    width: 210,
+    height: 210,
     borderRadius: 4,
     backgroundColor: '#ccc',
   },
